38189488ab05de925ec0485041972774a40f1b17
Darwinch123 <158114059+Darwinch123@users.noreply.github.com>
2024-02-14 02:36:15 -0600

    initial commit

---
+++ scrip1.js
@@ -0,0 +1,10 @@
+function Login() {
+    let usuario = document.getElementById('usuario').value;
+    let password = document.getElementById('contraseña').value;
+
+    if (usuario === "Darqui" && password === "14/02/2024") {
+        window.location = 'Inicio Love.html';
+    } else {
+        swal("Contraseña incorrecto","Intenta de nuevo mi dormilona hermosa","error");
+    }
+}
